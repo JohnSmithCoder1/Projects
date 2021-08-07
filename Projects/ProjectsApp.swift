e14df5634a3b187b4577e54c67cf8495d4e4e1d4
Banana Viking <bananavikingcoder@gmail.com>
2021-08-07 10:35:50 -0500

    Initial Commit

---
+++ Projects/ProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectsApp.swift
+//  Projects
+//
+//  Created by J S on 8/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
